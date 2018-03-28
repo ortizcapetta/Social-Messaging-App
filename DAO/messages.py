65e833498f9d9721d00c59b8e394ad66b3187f0c
ortizcapetta <alejandra.ortiz2@upr.edu>
2018-03-28 17:58:36 -0400

    built the message handler and routes fixed small mistakes in DAO

--- DAO/messages.py
+++ DAO/messages.py
@@ -6,19 +6,25 @@ class messagesDAO:
     def __init__(self):
         self.messages = []
         #check the date/time format, may be incorrect
-        message0 = [111, userDAO().users[0][0], groupsDAO().groups[0][0], datetime.deltatime(), 'example content1']
-        message1 = [222, userDAO().users[1][0], groupsDAO().groups[1][0], datetime.deltatime(), 'example content2']
-        message2 = [333, userDAO().users[2][0], groupsDAO().groups[2][0], datetime.deltatime(), 'example content3']
+        message0 = [111, userDAO().users[0][0], groupsDAO().groups[0][0], '2018-03-27 04:05:44', 'example content1']
+        message1 = [222, userDAO().users[1][0], groupsDAO().groups[1][0], '2018-03-27 05:10:45', 'example content2']
+        message2 = [333, userDAO().users[2][0], groupsDAO().groups[2][0], '2018-03-01 09:23:02', 'example content3']
+        message3 = [116, userDAO().users[1][0], groupsDAO().groups[0][0], '2018-03-27 04:15:44', 'Anyone online?']
         self.messages.append(message0)
         self.messages.append(message1)
         self.messages.append(message2)
+        self.messages.append(message3)
+
+    #returns all messages in the DB
+    def getMessages(self):
+        return self.messages
 
     #returns message content with matching ids
     def getMessageID(self, mid):
         result = []
         for a in self.messages:
             if mid == a[0]:
-                result.append(a[4])
+                result.append(a)
         return result
 
     #returns message id's with matching contents (strings only for now)
@@ -26,7 +32,7 @@ class messagesDAO:
         result = []
         for a in self.messages:
             if content == a[4]:
-                result.append(a[0])
+                result.append(a)
         return result
 
     #returns all message id's from group
@@ -34,7 +40,7 @@ class messagesDAO:
         result = []
         for a in self.messages:
             if gid == a[2]:
-                result.append(a[0])
+                result.append(a)
         return result
 
     #returns message time-stamp given ids (work in progress, need to test time/date datatypes)
@@ -42,7 +48,7 @@ class messagesDAO:
         result = []
         for a in self.messages:
             if mid == a[0]:
-                result.append(a[3])
+                result.append(a)
         return result
 
     #returns messages sent by a user id
@@ -50,5 +56,5 @@ class messagesDAO:
         result = []
         for a in self.messages:
             if uid == a[1]:
-                result.append(a[0])
+                result.append(a)
         return result
--- Handlers/messageshandler.py
+++ Handlers/messageshandler.py
@@ -3,4 +3,56 @@ from flask import *
 
 
 class MessagesHandler:
+    def buildMessageDict(self,row):
+        messages = {}
+        messages['mID']= row[0]
+        messages['sentBy'] = row[1]
+        messages['gID'] = row[2]
+        messages['timestamp'] = row[3]
+        messages['content'] = row[4]
+        return messages
 
+    def getMessages(self):
+        dao = messagesDAO()
+        messages = dao.getMessages()
+        message_list = []
+        for row in messages:
+            message_list.append(self.buildMessageDict(row))
+        return jsonify(Messages=message_list)
+
+    def getUserMessages(self,uid):
+        dao = messagesDAO()
+        messages = dao.getMessageSentBy(uid)
+        message_list = []
+        for row in messages:
+            message_list.append(self.buildMessageDict(row))
+
+        if message_list is None:
+            return jsonify(Error="User has sent no messages")
+        else:
+            return jsonify(Messages=message_list)
+
+
+    def getGroupMessages(self,gid):
+        dao = messagesDAO()
+        messages = dao.getGroupMessages(gid)
+        message_list = []
+        for row in messages:
+            message_list.append(self.buildMessageDict(row))
+
+        if message_list is None:
+            return jsonify(Error="No messages in group")
+        else:
+            return jsonify(Messages=message_list)
+
+    def getMessageID(self,mid):
+        dao = messagesDAO()
+        messages = dao.getMessageID(mid)
+        message_list = []
+        for row in messages:
+            message_list.append(self.buildMessageDict(row))
+
+        if message_list is None:
+            return jsonify(Error="No messages with that id")
+        else:
+            return jsonify(Messages=message_list)
