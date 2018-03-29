c7a2a644f4f5a46b66c728b484cfc63f453d3b7a
ortizcapetta <alejandra.ortiz2@upr.edu>
2018-03-28 21:51:28 -0400

    fix

--- DAO/gUsers.py
+++ DAO/gUsers.py
@@ -7,16 +7,18 @@ class gUsersDAO:
         user0 = [groupsDAO().groups[0][0], userDAO().users[0][0]]
         user1 = [groupsDAO().groups[1][0], userDAO().users[1][0]]
         user2 = [groupsDAO().groups[2][0], userDAO().users[2][0]]
+        user3 = [groupsDAO().groups[2][0],userDAO().users[0][0]]
         self.uInGroup.append(user0)
         self.uInGroup.append(user1)
         self.uInGroup.append(user2)
+        self.uInGroup.append(user3)
 
     #Get all groups of user with uid
     def getUserGroups(self, uid):
         result = []
         for a in self.uInGroup:
             if uid == a[1]:
-                result.append(a[0])
+                result.append(a)
         return result
 
     #Get all users in group with gid
@@ -24,5 +26,5 @@ class gUsersDAO:
         result = []
         for a in self.uInGroup:
             if gid == a[0]:
-                result.append(a[1])
+                result.append(a)
         return result
