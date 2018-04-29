8c17a9642c59dd3df2502b1c2db21a7ccb522622
Gustavo <gustavo.reyes1@upr.edu>
2018-04-29 18:18:21 -0400

    untested changes to UI, should load message list using REST API

--- UI/appjs/chat.js
+++ UI/appjs/chat.js
@@ -13,22 +13,46 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             //var url = "http://localhost:5000/blahblahblah/messages chatt app;
             //there's a way to do this as a singleton tho @_@
 
-            thisCtrl.messageList.push({"id": 1, "text": "Hola Mi Amigo", "author" : "Bob",
-            "like" : 4, "nolike" : 1});
-            thisCtrl.messageList.push({"id": 2, "text": "Hello World", "author": "Joe",
-                "like" : 11, "nolike" : 12});
+            //not completely sure how to handle url in case the route is hosted somewhere else
+            var url = "http://localhost:5000/users/messages";
+
+            $http.get(url).then(// success call back
+                function (response){
+                // The is the sucess function!
+                    console.log("response: " + JSON.stringify(response));
+                    thisCtrl.messageList = response.data.Messages;
+            }, // error callback
+            function (response){
+                // This is the error function
+                var status = response.status;
+                if (status == 0){
+                    alert("No hay conexion a Internet");
+                }
+                else if (status == 401){
+                    alert("Su sesion expiro. Conectese de nuevo.");
+                }
+                else if (status == 403){
+                    alert("No esta autorizado a usar el sistema.");
+                }
+                else if (status == 404){
+                    alert("No se encontro la informacion solicitada.");
+                }
+                else {
+                    alert("Error interno del sistema.");
+                }
+            });
 
             $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
         };
         //check partsapp ui for more stuff
         //callback de exito y callback de error
 
-
         this.postMsg = function(){
             var msg = thisCtrl.newText;
             // Need to figure out who I am
             var author = "Me";
             var nextId = thisCtrl.counter++;
+            // Might have to rename the fields here to be inline with our schema
             thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "author" : author, "like" : 0, "nolike" : 0});
             thisCtrl.newText = "";
         };
