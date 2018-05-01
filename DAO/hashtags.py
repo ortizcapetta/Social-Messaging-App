79b3592a9b6d83a04b42dd69c98e1199f24801f1
Gustavo Reyes <gustavo.reyes1@upr.edu>
2018-04-30 23:11:35 -0500

    hashtag/reactions DAO refactoring, handlers still need refactoring

--- DAO/hashtags.py
+++ DAO/hashtags.py
@@ -1,38 +1,48 @@
 from dbconfig import dbconfig
 import psycopg2
 
-class reactionsDAO:
+class hashtagsDAO:
     def __init__(self):
         curl = "dbname=%s user=%s password=%s" % (dbconfig['dbname'],
                                                      dbconfig['user'],
                                                      dbconfig['password'])
         self.connection = psycopg2._connect(curl)
 
-    #Get all messages user has reacted to, 0 for dislike, 1 for like
-    def getUserReactions(self, uid):
+    #Get all hashtags
+    def getHashtags(self):
         cursor = self.connection.cursor()
-        query = "select * from Reactions where uid = %s;"
-        cursor.execute(query,(uid,))
+        query = "select * from Hashtags;"
+        cursor.execute(query)
         result = []
         for row in cursor:
             result.append(row)
         return result
-
-    #Get reactions to a message
-    def getReactions(self, mid):
+    
+    #Get hashtags by mID
+    def getMessageHashtags(self, mid):
         cursor = self.connection.cursor()
-        query = "select * from Reactions where mid = %s;"
+        query = "select * from Hashtags where mid = %s;"
         cursor.execute(query,(mid,))
         result = []
         for row in cursor:
             result.append(row)
         return result
 
-    #Get reactions with rID
-    def getReactionsId(self, rid):
+    #Get hashtags by content
+    def getContentHashtags(self, hashtag):
         cursor = self.connection.cursor()
-        query = "select * from Reactions where rid = %s;"
-        cursor.execute(query,(rid,))
+        query = "select * from Hashtags where hashtag = %s;"
+        cursor.execute(query,(hashtag,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    #Get hashtags by htID
+    def getIdHashtags(self, htid):
+        cursor = self.connection.cursor()
+        query = "select * from Hashtags where htid = %s;"
+        cursor.execute(query,(htid,))
         result = []
         for row in cursor:
             result.append(row)
