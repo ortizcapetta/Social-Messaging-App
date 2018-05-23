19c091203f7a8bfde9d30c9c270f17b736db99f0
ortizcapetta <alejandra.ortiz2@upr.edu>
2018-05-23 11:30:43 -0400

    UI testing

--- UI/appjs/main.js
+++ UI/appjs/main.js
@@ -8,12 +8,28 @@
             templateUrl: 'pages/login.html',
             controller: 'LoginController',
             controllerAs : 'logingCtrl'
+
+             }).when('/groups', { //detail from specific part, i can add /chat/gid or smthng
+            templateUrl: 'pages/chatgroups.html',
+            controller: 'GroupChatController',
+            controllerAs : 'groupChatCtrl' //controlers
+             }).when('/groups/:gid/messages', {
+            templateUrl: 'pages/messages.html',
+            controller: 'MessageController',
+            controllerAs : 'msgCtrl' //controlers
+
+
+       /*
         }).when('/chat', { //detail from specific part, i can add /chat/gid or smthng
             templateUrl: 'pages/chat.html',
             controller: 'ChatController',
             controllerAs : 'chatCtrl' //controlers
+            */
+
+
+
         }).otherwise({ //default routes
-            redirectTo: '/chat'
+            redirectTo: '/groups'
         });
     }]);
 })();
