673bb84bab483936e25e55a02c6c0024383c0db3
ortizcapetta <alejandra.ortiz2@upr.edu>
2018-05-31 12:52:39 -0400

    create chat group html angular

--- UI/appjs/gc.js
+++ UI/appjs/gc.js
@@ -56,6 +56,46 @@ angular.module('AppChat').controller('GroupChatController', ['$http', '$log', '$
             $log.error("Groups Loaded: ", JSON.stringify(thisCtrl.groupList));
         };
 
+ this.createNewGroup = function(gName){
+            // Get the target part id from the parameter in the url
+            // using the $routerParams object
+            var gid = $routeParams.gid;
+            var data = {'gName':gName}
+
+            console.log("data:" + JSON.stringify(data));
+
+            // Now create the url with the route to talk with the rest API
+            var url = "http://localhost:5000/users/"+thisCtrl.loggedUser[0].uID+"/groups";
+            console.log("reqURL: " + url);
+            // Now issue the http request to the rest API
+            $http.post(url,data).then(
+                // Success function
+                function (response) {
+                     console.log("data: " + JSON.stringify(response.data));
+                }, //Error function
+                function (response) {
+
+                    var status = response.status;
+
+                    if (status == 0) {
+                        alert("No hay conexion a Internet");
+                    }
+                    else if (status == 401) {
+                        alert("Su sesion expiro. Conectese de nuevo.");
+                    }
+                    else if (status == 403) {
+                        alert("No esta autorizado a usar el sistema.");
+                    }
+                    else if (status == 404) {
+                        alert("No se encontro la informacion solicitada.");
+                    }
+                    else {
+                        alert("Error interno del sistema.");
+                    }
+                }
+            );
+        };
+
 
         this.viewGroup = function (gid) {
             $location.url('/groups/' + gid + '/messages');
